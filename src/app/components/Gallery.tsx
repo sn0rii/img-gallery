c489790551b07f9fa78198b8e1415543b3e8b607
Wojtek M <ojciecneo@gmail.com>
2023-11-03 17:18:29 +0100

    footer completed + prevPage problem sorted

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -2,28 +2,47 @@ import fetchImages from "@/lib/fetchImages";
 import type { ImagesResults } from "@/models/Images";
 import ImgContainer from "./ImgContainer";
 import addBlurredDataUrls from "@/lib/getBase64";
+import getPrevNextPages from "@/lib/getPrevNextPages";
+import Footer from "./Footer";
 
 type Props = {
   topic?: string | undefined;
+  page?: string | undefined;
 };
 
-export default async function Gallery({ topic }: Props) {
-  const url = !topic
-    ? "https://api.pexels.com/v1/curated"
-    : `https://api.pexels.com/v1/search?query=${topic}`;
+export default async function Gallery({ topic = "curated", page }: Props) {
+  let url;
+  if (topic === "curated" && page) {
+    url = `https://api.pexels.com/v1/curated?page=${page}`;
+  } else if (topic === "curated") {
+    url = "https://api.pexels.com/v1/curated";
+  } else if (!page) {
+    url = `https://api.pexels.com/v1/search?query=${topic}`;
+  } else {
+    url = `https://api.pexels.com/v1/search?query=${topic}&page=${page}`;
+  }
+
+  //&locale=pl-pl
 
   const images: ImagesResults | undefined = await fetchImages(url);
 
-  if (!images)
+  if (!images || images.per_page === 0)
     return <h2 className="m-4 text-2xl font-bold">No Images Found</h2>;
 
   const photosWithBlur = await addBlurredDataUrls(images);
 
+  const { prevPage, nextPage } = getPrevNextPages(images);
+
+  const footerProps = { topic, page, nextPage, prevPage };
+
   return (
-    <section className="px-1 my-3 grid grid-cols-gallery auto-rows-[10px]">
-      {photosWithBlur.map((photo) => (
-        <ImgContainer key={photo.id} photo={photo} />
-      ))}
-    </section>
+    <>
+      <section className="px-1 my-3 grid grid-cols-gallery auto-rows-[10px]">
+        {photosWithBlur.map((photo) => (
+          <ImgContainer key={photo.id} photo={photo} />
+        ))}
+      </section>
+      <Footer {...footerProps} />
+    </>
   );
 }
